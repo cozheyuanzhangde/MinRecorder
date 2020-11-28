4afedf6a690a2bc24498bfb9f5d1968c2ccfe519
cozzyde <54963496+cozheyuanzhangde@users.noreply.github.com>
2020-11-28 16:37:40 +0800

    initial

---
+++ MinRecorder/MinRecorder/ScreenRecorder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace MinRecorder
+{
+    class ScreenRecorder
+    {
+        //Video variables:
+        private Rectangle bounds;          //bounds of the screen
+        private string outputPath = "";    //final video location/path
+        private string tempPath = "";      //temp location/path to store screenshots
+        private int fileCount = 1;
+        private List<string> inputImageSequence = new List<string>();
+
+        //File variables:
+
+    }
+}
